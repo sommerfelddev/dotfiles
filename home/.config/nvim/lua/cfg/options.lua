edaf1132d385203899d9841bed7a403782610c2c
sommerfeld <sommerfeld@sommerfeld.dev>
2024-05-22 17:23:05 +0100

    [nvim] Remove legacy plugin disabling
    lazy.nvim does it by default.

--- home/.config/nvim/lua/cfg/options.lua
+++ home/.config/nvim/lua/cfg/options.lua
@@ -58,26 +58,6 @@ vim.g.python_host_prog = 0
 vim.g.python3_host_prog = 0
 vim.g.netrw_home = vim.fn.stdpath("data")
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
-vim.g.loaded_netrwFileHandlers = 1
-vim.g.loaded_gzip = 1
-vim.g.loaded_zip = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_tar = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_getscript = 1
-vim.g.loaded_getscriptPlugin = 1
-vim.g.loaded_vimball = 1
-vim.g.loaded_vimballPlugin = 1
-vim.g.loaded_2html_plugin = 1
-vim.g.loaded_logipat = 1
-vim.g.loaded_rrhelper = 1
-vim.g.loaded_matchparen = 1
-vim.g.loaded_spec = 1
-
 opt.diffopt:append({
   ["indent-heuristic"] = true,
   hiddenoff = true,
