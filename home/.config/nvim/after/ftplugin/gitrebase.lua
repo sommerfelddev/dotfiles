7c3bb64355139097395e3e13b784e83c8b684ebc
sommerfeld <sommerfeld@sommerfeld.dev>
2025-10-24 11:48:23 +0100

    nvim: Add missing <CR> to mappings

--- home/.config/nvim/after/ftplugin/gitrebase.lua
+++ home/.config/nvim/after/ftplugin/gitrebase.lua
@@ -1,5 +1,10 @@
 local function nvmap(l, r, desc)
-  vim.keymap.set({ "n", "v" }, l, r, { buffer = 0, desc = desc })
+  vim.keymap.set(
+    { "n", "v" },
+    l,
+    ":" .. r .. "<CR>",
+    { buffer = 0, desc = "[G]it rebase " .. desc }
+  )
 end
 
 nvmap("gc", "Cycle", "[C]ycle")
