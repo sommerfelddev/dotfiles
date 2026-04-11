ebbebe9ae672c0f65f1ffca65b97570a38d68072
sommerfeld <sommerfeld@sommerfeld.dev>
2026-04-11 16:04:35 +0100

    nvim: replace indent-line with blink.indent

--- home/.config/nvim/lua/custom/plugins/ui.lua
+++ home/.config/nvim/lua/custom/plugins/ui.lua
@@ -9,41 +9,34 @@ return {
     end,
   },
   {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "BufRead",
-    config = function()
-      local highlight = {
-        "RainbowRed",
-        "RainbowYellow",
-        "RainbowBlue",
-        "RainbowOrange",
-        "RainbowGreen",
-        "RainbowViolet",
-        "RainbowCyan",
-      }
-      local hooks = require("ibl.hooks")
-      -- create the highlight groups in the highlight setup hook, so they are reset
-      -- every time the colorscheme changes
-      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-      end)
-
-      vim.g.rainbow_delimiters = { highlight = highlight }
-      require("ibl").setup({
-        indent = { highlight = highlight },
-      })
-
-      hooks.register(
-        hooks.type.SCOPE_HIGHLIGHT,
-        hooks.builtin.scope_highlight_from_extmark
-      )
-    end,
+    "saghen/blink.indent",
+    --- @module 'blink.indent'
+    --- @type blink.indent.Config
+    opts = {
+      scope = {
+        highlights = {
+          "BlinkIndentOrange",
+          "BlinkIndentViolet",
+          "BlinkIndentBlue",
+          "BlinkIndentRed",
+          "BlinkIndentCyan",
+          "BlinkIndentYellow",
+          "BlinkIndentGreen",
+        },
+        underline = {
+          enabled = true,
+          highlights = {
+            "BlinkIndentOrangeUnderline",
+            "BlinkIndentVioletUnderline",
+            "BlinkIndentBlueUnderline",
+            "BlinkIndentRedUnderline",
+            "BlinkIndentCyanUnderline",
+            "BlinkIndentYellowUnderline",
+            "BlinkIndentGreenUnderline",
+          },
+        },
+      },
+    },
   },
   {
     "nvim-lualine/lualine.nvim",
