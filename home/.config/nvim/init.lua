fcab70e06325f123030a9442aef001707a603950
sommerfeld <sommerfeld@sommerfeld.dev>
2026-04-17 10:53:11 +0100

    refactor: flatten custom/plugins/ to plugins/
    Rename lua/custom/plugins/ to lua/plugins/ following the standard lazy.nvim
    convention. The extra custom/ nesting added no value. Update import path in
    init.lua accordingly.

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -30,7 +30,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ import = "custom/plugins" }, {
+require("lazy").setup({ import = "plugins" }, {
   defaults = {
     version = "*",
   },
