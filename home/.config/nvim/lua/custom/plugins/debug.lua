755a3fcebe80cd02cee5eac669d6df770f0af7ad
sommerfeld <sommerfeld@sommerfeld.dev>
2026-04-17 10:53:07 +0100

    chore: remove commented-out debug configurations
    Remove ~80 lines of commented-out GDB, LLDB, dap-ui, and dap-view configs.
    These were alternative configurations tried and abandoned. They can be
    recovered from git history if needed.

--- home/.config/nvim/lua/custom/plugins/debug.lua
+++ home/.config/nvim/lua/custom/plugins/debug.lua
@@ -66,27 +66,6 @@ return {
       end
 
       dap.configurations.cpp = {
-        -- {
-        --   name = "GDB Launch",
-        --   type = "gdb",
-        --   request = "launch",
-        --   cwd = "${workspaceFolder}",
-        --   program = get_program,
-        --   args = get_args,
-        --   env = get_env_vars,
-        --   stopAtBeginningOfMainSubprogram = false,
-        -- },
-        -- {
-        --   name = "LLDB Launch",
-        --   type = "lldb",
-        --   request = "launch",
-        --   cwd = "${workspaceFolder}",
-        --   program = get_program,
-        --   args = get_args,
-        --   env = get_env_vars,
-        --   stopOnEntry = true,
-        --   disableASLR = false,
-        -- },
         {
           name = "codelldb Launch",
           type = "codelldb",
@@ -97,99 +76,12 @@ return {
           stopOnEntry = true,
           console = "integratedTerminal",
         },
-        -- {
-        --   name = "GDB Attach to process",
-        --   type = "gdb",
-        --   request = "attach",
-        --   pid = require('dap.utils').pick_process,
-        -- },
-        -- {
-        --   name = "LLDB Attach to process",
-        --   type = "lldb",
-        --   request = "attach",
-        --   pid = require('dap.utils').pick_process,
-        -- },
-        -- {
-        --   name = "codelldb Attach to process",
-        --   type = "codelldb",
-        --   request = "attach",
-        --   pid = require('dap.utils').pick_process,
-        -- },
       }
 
       dap.configurations.c = dap.configurations.cpp
       dap.configurations.rust = dap.configurations.cpp
-
-      -- local dapui = require("dapui")
-      -- dap.listeners.before.attach.dapui_config = dapui.open
-      -- dap.listeners.before.launch.dapui_config = dapui.open
-      -- dap.listeners.before.event_terminated.dapui_config = dapui.close
-      -- dap.listeners.before.event_exited.dapui_config = dapui.close
-
-      -- local dv = require("dap-view")
-      -- dap.listeners.before.attach["dap-view-config"] = dv.open
-      -- dap.listeners.before.launch["dap-view-config"] = dv.open
-      -- dap.listeners.before.event_terminated["dap-view-config"] = dv.close
-      -- dap.listeners.before.event_exited["dap-view-config"] = dv.close
     end,
     dependencies = {
-      -- {
-      --   "igorlfs/nvim-dap-view",
-      --   keys = {
-      --     {
-      --       "<leader>td",
-      --       function()
-      --         require("dap-view").toggle(true)
-      --       end,
-      --       desc = "[T]oggle [D]ebug UI",
-      --     },
-      --   },
-      --   opts = {},
-      -- },
-      -- {
-      --   "rcarriga/nvim-dap-ui",
-      --   dependencies = "nvim-neotest/nvim-nio",
-      --   keys = {
-      --     {
-      --       "<leader>td",
-      --       function()
-      --         require("dapui").toggle()
-      --       end,
-      --       desc = "[T]oggle [D]ebug UI",
-      --     },
-      --     {
-      --       "<leader>de",
-      --       function()
-      --         require("dapui").eval()
-      --       end,
-      --       desc = "[D]ebug [E]valuate",
-      --     },
-      --   },
-      --   opts = {
-      --     icons = { expanded = "-", collapsed = "+", current_frame = "*" },
-      --     controls = { enabled = false },
-      --     layouts = {
-      --       {
-      --         elements = {
-      --           -- Elements can be strings or table with id and size keys.
-      --           "scopes",
-      --           "breakpoints",
-      --           "stacks",
-      --           "watches",
-      --         },
-      --         size = 40,
-      --         position = "left",
-      --       },
-      --       {
-      --         elements = {
-      --           "repl",
-      --         },
-      --         size = 0.25, -- 25% of total lines
-      --         position = "bottom",
-      --       },
-      --     },
-      --   },
-      -- },
       {
         "theHamsta/nvim-dap-virtual-text",
         opts = {},
